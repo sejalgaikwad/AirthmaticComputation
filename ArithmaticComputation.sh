648b697f94b9d7aa521b28f21f4800a6c8f19b10
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 17:16:05 +0530

    Added: solveing second input

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -6,5 +6,10 @@ read -p "Enter First Number:-" a
 read -p "Enter Second Number:-" b
 read -p "Enter Third Number:-" c
 
-result=$(($a+$b*$c))
-echo "result:- "$result 
+eq1=$(($a+$b*$c))
+
+eq2=$(($a*$b+$c))
+
+echo "Equation1:- "$eq1 
+
+echo "Equation2:- "$eq2
