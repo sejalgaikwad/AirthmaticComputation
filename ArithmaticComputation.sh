2613318a10e5129637cf485fc8c8e6a55d7172f6
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 17:37:58 +0530

    Added: result in Dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,7 @@
 
 echo "Welcome to Arithmatic Computation Sorting "
 
+declare -A resultDictionary
 read -p "Enter First Number:-" a
 read -p "Enter Second Number:-" b
 read -p "Enter Third Number:-" c
@@ -10,9 +11,9 @@ eq1=$(($a+$b*$c))
 
 eq2=$(($a*$b+$c))
 
-eq3=$(($c+$a/$b)
+eq3=$(($c+$a/$b))
 
-eq4=$(($a%$b-$c)
+eq4=$(($a%$b-$c))
 
 echo "Equation1:- "$eq1 
 
@@ -21,3 +22,13 @@ echo "Equation2:- "$eq2
 echo "Equation3:- "$eq3
 
 echo "Equation4:- "$eq4
+
+resultDictionary[0]=$eq1
+
+resultDictionary[1]=$eq2
+
+resultDictionary[2]=$eq3
+
+resultDictionary[3]=$eq4
+
+
