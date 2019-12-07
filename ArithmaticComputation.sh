2156a2e4d64087c5928b93dc575e328bd86fbc03
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 17:04:26 +0530

    Added: userInputs

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,6 @@
 
 echo "Welcome to Arithmatic Computation Sorting "
 
-#read -p "Enter First Number:-" a
-#read -p "Enter Second Number:-" b
-#read -p "Enter Third Number:-" c
+read -p "Enter First Number:-" a
+read -p "Enter Second Number:-" b
+read -p "Enter Third Number:-" c
