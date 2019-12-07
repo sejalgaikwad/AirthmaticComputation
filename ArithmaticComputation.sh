86aa015bebb3d8dcf211c706fa72c8dbc00c8d12
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 17:09:26 +0530

    Added: First Computation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,3 +5,6 @@ echo "Welcome to Arithmatic Computation Sorting "
 read -p "Enter First Number:-" a
 read -p "Enter Second Number:-" b
 read -p "Enter Third Number:-" c
+
+result=$(($a+$b*$c))
+echo "result:- "$result 
